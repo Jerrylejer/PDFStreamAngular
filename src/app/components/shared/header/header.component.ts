095ca50d2ba2a1c16934ecaea5c0c76a02e5f75d
Jerrylejer <jerrylejer23@yahoo.fr>
2024-02-08 16:37:08 +0100

    Gestion de la persistence de l'état authentifié  au changement de page, utilisation du localStorage pour renseigner de cet état, gestion de la déconnexion avec la fonction logout() qui utilise le authService - deconnexion, modification du html et appel à la méthode au (click) ok

--- src/app/components/shared/header/header.component.ts
+++ src/app/components/shared/header/header.component.ts
@@ -11,20 +11,45 @@ import { AuthService } from 'src/app/services/auth/auth.service';
 export class HeaderComponent implements OnInit {
   // ReactiveForms
   authForm!: FormGroup;
-  //FormBuilder
+  //Injections de FormBuilder + AuthService
   constructor(private formBuilder: FormBuilder, private auth: AuthService) {};
 
-  // Initialisation du formulaire
+  // ###################### Variables utilisées ######################
+  // Boolean pour rendre la modale visible ou non au click "connexion", "X"
+  // [ngStyle]="{'display':displayStyle}
+  displayStyle = "none";
+  openModale() {
+    this.displayStyle = "block";
+  }
+  closeModale() {
+    this.displayStyle = "none";
+  }
+  // Configuration du message d'erreur si connexion failed
+  errorMessage: String = '';
+  // Boolean => switcher l'affichage de la nav (connexion/inscription => déconnexion/compte)
+  isConnected: boolean = false;
+  // ##################################################################
+
+  // Initialisation du formulaire et des critères de validation quant au remplissage des champs
   ngOnInit() {
     this.authForm = this.formBuilder.group({
       username: ['', Validators.required],
       password: ['', Validators.required]
-    })
+    });
+    this.isLoggedInPersistence();
+  }
+
+  // Persistence de l'état connecté grâce au LocalStorage
+  isLoggedInPersistence() {
+    const isAuthenticated = localStorage.getItem("isAuthenticated");
+    if(isAuthenticated && isAuthenticated === 'true') {
+      this.isConnected = true;
+    } else {
+      this.isConnected = false;
+    }
   }
   
-  // Une props pour le paramétrage des messages d'erreurs à la validation
-  errorMessage: String = '';
-  
+  // Soumission du formulaire
   submit() {
     if(this.authForm.valid) {
       const username = this.authForm.value.username;
@@ -41,24 +66,31 @@ export class HeaderComponent implements OnInit {
         .subscribe(
           (response) => {
             console.log("authentification réussie")
-            // Si la connexion est ok, je ferme ma modale
+            // Si la connexion est ok, je ferme ma modale et je modifie l'affichage de la nav
             this.displayStyle = "none";
             this.isConnected = true;
+            // Modification de la valeur pour la clé "isAuthenticated" dans le localStorage
+            localStorage.setItem('isAuthenticated', 'true');
           }
         );
     }
   }
 
-  // Boolean pour rendre la modale visible ou non au click "connexion", "X"
-  displayStyle = "none";
-  // Boolean pour switcher les valeurs de la nav (déconnecté/connecté)
-  isConnected: boolean = false;
-
-  openModale() {
-    this.displayStyle = "block";
-  }
-
-  closeModale() {
-    this.displayStyle = "none";
+  logout() {
+    this.auth.deconnexion()
+    .pipe(
+      catchError((error) => {
+        console.log("erreur lors de la déconnexion : ", error);
+        return throwError(() => error)
+      })
+    )
+    .subscribe(
+      (response) => {
+        console.log("Déconnexion réussie")
+        // Modification de la valeur pour la clé "isAuthenticated" dans le localStorage
+        localStorage.setItem('isAuthenticated', 'false');
+        this.isConnected = false;
+      }
+    )
   }
 }

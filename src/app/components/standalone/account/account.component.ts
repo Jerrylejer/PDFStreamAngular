4c278b45686d0c26996fbff7abd08bc74c6af821
Jerrylejer <jerrylejer23@yahoo.fr>
2024-02-10 21:56:44 +0100

    composant account pour le display des données de compte d'un user. Mise en place d'un form fixe et bloqué qui affichera les datas de la bdd, mise en place d'une modale qui intègre un form pour l'update des datas, openModal(), closeModale(), deleteAccount() et submit() à développer + logique service pour affichedr les datas et ensuite pour les updater à développer, voir css responsive

--- src/app/components/standalone/account/account.component.ts
+++ src/app/components/standalone/account/account.component.ts
@@ -1,10 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { UserService } from 'src/app/services/user/user.service';
 
 @Component({
   selector: 'app-account',
   templateUrl: './account.component.html',
   styleUrl: './account.component.css'
 })
-export class AccountComponent {
+export class AccountComponent implements OnInit{
 
+  updateForm!: FormGroup;
+
+  constructor(private formBuilder: FormBuilder, private userService: UserService){};
+
+  // ###################### Variables utilisées ######################
+  // Boolean pour rendre la modale visible ou non au click "connexion", "X"
+  // [ngStyle]="{'display':displayStyle}
+  displayStyle = "none";
+  openModale() {
+    this.displayStyle = "block";
+  }
+  closeModale() {
+    this.displayStyle = "none";
+  }
+
+  ngOnInit(): void {
+      this.updateForm = this.formBuilder.group({
+
+      })
+  }
+
+
+  deleteAccount() {
+
+  }
+
+  submit() {
+
+  }
 }

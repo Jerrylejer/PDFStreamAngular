a92ae790ca1b12d869e4d60db01c1856cb5274f4
Jerrylejer <jerrylejer23@yahoo.fr>
2024-02-22 18:22:58 +0100

    Modification pour l'update du pdf, je passe par le ReactiveForm et affiche avantg les données existantes dans le formulaire

--- src/app/components/standalone/shares/shares.component.ts
+++ src/app/components/standalone/shares/shares.component.ts
@@ -7,6 +7,7 @@ import { Router } from '@angular/router';
 import { catchError, throwError } from 'rxjs';
 import { Category } from 'src/app/models/category.model';
 import { Pdf } from 'src/app/models/pdf.model';
+import { CategoryService } from 'src/app/services/category/category.service';
 import { PdfService } from 'src/app/services/pdf/pdf.service';
 import { UserService } from 'src/app/services/user/user.service';
 
@@ -16,30 +17,12 @@ import { UserService } from 'src/app/services/user/user.service';
   styleUrl: './shares.component.css'
 })
 export class SharesComponent implements OnInit{
-// Material Tables configuration
+
+// MATERIAL TABLES
 displayedColumns: String[] = ['title', 'createdAt', 'evaluation','config'];
 dataSource!: MatTableDataSource<Pdf>; 
 @ViewChild(MatPaginator) paginator!: MatPaginator;
 @ViewChild(MatSort) sort!: MatSort;
-
-// MODALES
-// Boolean pour rendre la modale création visible ou non au click "ajouter"
-displayCreationModale = "none";
-displayDeleteModale = "none";
-openCreationModale() {
-  this.displayCreationModale = "block";
-  this.resetPdfForm();
-}
-openDeleteModale() {
-  this.displayDeleteModale = "block";
-}
-closeCreationModale() {
-  this.displayCreationModale = "none";
-}
-closeDeleteModale() {
-  this.displayDeleteModale = "none";
-}
-
 // Filtre de recherche
 applyFilter(event: Event) {
   const filterValue = (event.target as HTMLInputElement).value;
@@ -50,32 +33,95 @@ applyFilter(event: Event) {
   }
 }
 
+// MODALES
+// Boolean pour rendre la modale création visible ou non au click "ajouter"
+displayCreationModale = "none";
+displayUpdateModale = "none";
+displayDeleteModale = "none";
+pdfToUpdate: Pdf = {};
+pdfToDelete: Pdf = {};
+
+openCreationModale() {
+  this.displayCreationModale = "block";
+  // this.resetPdfForm();
+}
+
+openUpdateModale(pdf: any) {
+  this.displayUpdateModale = "block";
+  // Je passe l'objet à ma prop de class "pdfToUpdate", objet que j'utilise dans updatePdf()
+  this.pdfToUpdate = pdf;
+  // J'affiche les caractéristiques du pdf à modifier dans mon formulaire (callback)
+  this.showPdf(pdf);
+}
+
+openDeleteModale(pdf: any) {
+  this.displayDeleteModale = "block";
+  // Je passe l'objet à ma prop de class "pdfToDelete", objet que j'utilise dans deletePdf()
+  this.pdfToDelete = pdf;
+}
+
+closeCreationModale() {
+  this.displayCreationModale = "none";
+}
+
+closeUpdateModale() {
+  this.displayUpdateModale = "none";
+}
+
+closeDeleteModale() {
+  this.pdfToDelete = {};
+  this.displayDeleteModale = "none";
+}
+
 // Booléan pour switcher du mode formulaire / formulaire soumis
 submitted = false;
 
-// Interface qui reçoit les données du formulaire
-pdf: Pdf = {
-  title: '',
-  createdAt: new Date(),
-  evaluations: [],
-}
-
-// Formulaire
-pdfForm!: FormGroup;
-// Propriété de class qui stocke la liste des pdfs partagés par le user
+// LISTE DES PDFS
 uploadedPdf?: Pdf[] = []; 
-// Propriété de class qui stocke les différentes catégories paramétrées en bdd
+
+// LISTE DES CATEGORIES
 allCategories: Category[] = [];
+
 // Récupération de l'id dans le localStorage
 id = Number(localStorage.getItem('userId'));
 
 constructor(private pdfService: PdfService,
   private router: Router,
-  // private formBuilder: FormBuilder,
-  private userService: UserService) {}
+  private formBuilder: FormBuilder,
+  private userService: UserService,
+  private categoryService: CategoryService) {}
 
-ngOnInit(): void {
-  // Il faut que je récupère l'objet user 
+  // FORMULAIRE DE CREATION
+  createForm!: FormGroup;
+  // FORMULAIRE UPDATE
+  updateForm!: FormGroup;
+  
+  ngOnInit(): void {
+
+  // Initialisation du formulaire de creation
+  this.createForm = this.formBuilder.group({
+    author: ['', Validators.required], // invisible client ou non accessible
+    pdfTitle:['', Validators.required],
+    pdfSmallDesc: ['', Validators.required],
+    pdfDesc: ['', Validators.required],
+    pdfImage: ['', Validators.required],
+    pdfFile: ['', Validators.required],
+    pdfCategory: ['', Validators.required]
+  });
+
+  // Initialisation du formulaire d'update
+  this.updateForm = this.formBuilder.group({
+    author: ['', Validators.required], // invisible client ou non accessible
+    pdfUpdatedTitle: [''],
+    pdfUpdatedSmallDesc: [''],
+    pdfUpdatedDesc: [''],
+    pdfUpdatedImage: [''],
+    pdfUpdatedFile: [''],
+    pdfUpdatedCategory: ['']
+  });
+
+  // Récupération des pdfs d'un User connecté
+  // Requête de récupération de l'objet user
   this.userService.getUserById(this.id).pipe(
     catchError((error) => {
       return throwError(() => error);
@@ -83,9 +129,9 @@ ngOnInit(): void {
     )
     .subscribe({
       next: (user) => {
-        // J'initialise "uploadedPdf" avec les pdfs du user
+        // J'initialise "uploadedPdf" avec les pdfs du user (j'utilise les props de l'objet)
         this.uploadedPdf = user.pdfs;
-        // Je les envoie dans MatTable
+        // et je les envoie dans MatTable
         this.dataSource = new MatTableDataSource(this.uploadedPdf);
         this.dataSource.paginator = this.paginator;
         this.dataSource.sort = this.sort;
@@ -93,83 +139,130 @@ ngOnInit(): void {
       },
       error: (e) => console.error(e)
     });
+
+    // Récupération de toutes les Catégories
+    this.categoryService.getCategorieslist().pipe(
+      catchError((error) => {
+        return throwError(() => error);
+      })
+    )
+    .subscribe((response) => {
+      this.allCategories = response;
+    }
+    )
   }
   
   // ######################################################################
   // ############### METHODES ADMINISTRATION DES PDFS #####################
   // ######################################################################
   
-  // Interface céation d'un nouveau pdf
-  pdfToCreate: Pdf = {
-    title: '',
-    smallDescription: '',
-    description:'',
-    image: '',
-    categories: []
-  }
-  
-  // Interface modification d'un pdf
-  pdfToUpdate: Pdf = {
-    title: '',
-    smallDescription: '',
-    description:'',
-    image: '',
-    categories: []
-  }
-  
-  // Remise à 0 du formulaire
-  resetPdfForm(): void {
-    this.submitted = false;
-    this.pdf = {
-      title: '',
-      smallDescription: '',
-      description:'',
-      image: '',
-      categories: []
+  // Création d'un pdf
+  createPdf() {
+    const pdfTitle: any = this.createForm.value.pdfTitle;
+    const pdfSmallDesc: any = this.createForm.value.pdfSmallDesc;
+    const pdfDesc: any = this.createForm.value.pdfDesc;
+    const pdfImage: any = this.createForm.value.pdfImage;
+    const pdfFile: any = this.createForm.value.pdfFile;
+    const pdfCategory: any = this.createForm.value.pdfCategory;
+    // Appel à mon service
+    this.pdfService.uploadPdf(pdfTitle, pdfSmallDesc, pdfDesc, pdfImage, pdfFile, pdfCategory).pipe(
+      catchError((error) => {
+        console.log("erreur mon ptit gars!");
+        //this.ngOnInit();
+        return throwError(() => error);
+      })
+    )
+    .subscribe(
+      (response) => {console.log(response, "datas modifiées !"),
+    this.closeUpdateModale();
+    this.ngOnInit();
     }
+    )
   }
   
   // Suppression d'un pdf
-  deletePdf(pdf: any) {
-    const pdfId: number = pdf.id;
-    const pdfTitle: String = pdf.title;
-    if (confirm(`Voulez-vous supprimer le poste ${pdfTitle} ?`)) {
-      console.log(pdfId);
+  deletePdf() {
+    const pdfId: any = Number(this.pdfToDelete.id);
       this.pdfService.deletePdf(pdfId).subscribe({
         next: (res) => {
-          console.log(res);
+          this.closeDeleteModale();
           this.ngOnInit();
         },
         error: (e) => console.error(e)
       });
     }
-  }
   
-  // Sélectionner et afficher les datas du pdf dans la modale d'update
-  selectedPdf(pdf: any) {
-    // Je récupère le bon pdf par l'id et je passe les datas à mon objet "filmToUpdate"
-    this.pdfService.getPdfById(pdf.id).subscribe({
-      // Je récupère les données issuent du pdf sélectionné et les initialise dans mon interface "pdfToUpdate"
-      // J'affiche ainsi dans ma modale d'update, les données du pdf sélectionné avec [(ngModel)]
-      next: (selectedPdf) => {
-        this.pdfToUpdate.id = selectedPdf.id;
-        this.pdfToUpdate.title = selectedPdf.title;
-        this.pdfToUpdate.smallDescription = selectedPdf.smallDescription;
-        this.pdfToUpdate.description = selectedPdf.description;
-        this.pdfToUpdate.image = selectedPdf.image;
-        this.pdfToUpdate.categories = selectedPdf.categories;
-      },
-      error: (e) => console.error(e)
-    })
+  // I - UPDATE DU PDF -> Afficher le pdf sélectionné
+  // Récupération des datas this.pdfService.getPdfById(pdf.id)
+  // puis valeurs communiquées au Form d'update
+  pdfAuthor: any = '';
+  pdfTitle: any = ''; 
+  pdfSmallDesc: any = ''; 
+  pdfDesc: any = ''; 
+  pdfCategory: any = ''; 
+
+ showPdf(pdf: any) {
+    // Je requête le pdf par son id 
+    this.pdfService.getPdfById(pdf.id).pipe(
+      catchError((error) => {
+        return throwError(() => error);
+      })
+    )
+    .subscribe(
+      (response) => {
+        console.log(response)
+        // Je récupère les données du pdf, ok
+        this.pdfAuthor = response.author
+        this.pdfTitle = response.title;
+        this.pdfSmallDesc = response.smallDescription;
+        this.pdfDesc = response.description;
+        this.pdfCategory = response.categories
+        }
+    )
   }
-  
+  // II - UPDATE DU PDF -> modifier le PDF
+  updatePdf() {
+    const pdfId: any = Number(this.pdfToUpdate.id);
+
+    const formData: FormData = new FormData();
+    const formValues = this.updateForm.value;
+
+  if (formValues.pdfUpdatedTitle !== '') {
+      formData.append('title', formValues.pdfUpdatedTitle);
+      console.log(formValues.pdfUpdatedTitle)
+  }
+  if (formValues.pdfUpdatedSmallDesc !== '') {
+      formData.append('smallDescription', formValues.pdfUpdatedSmallDesc);
+      console.log(formValues.pdfUpdatedSmallDesc)
+  }
+  if (formValues.pdfUpdatedDesc !== '') {
+      formData.append('description', formValues.pdfUpdatedDesc);
+      console.log(formValues.pdfUpdatedDesc)
+  }
+  if (formValues.pdfUpdatedImage) {
+      formData.append('image', formValues.pdfUpdatedImage);
+      console.log(formValues.pdfUpdatedImage)
+  }
+  if (formValues.pdfUpdatedFile) {
+      formData.append('pdfFile', formValues.pdfUpdatedFile);
+      console.log(formValues.pdfUpdatedFile)
+  }
+  if (formValues.pdfUpdatedCategory !== '') {
+      formData.append('category', formValues.pdfUpdatedCategory);
+      console.log(formValues.pdfUpdatedCategory)
+  }
+    this.pdfService.updatePdf(pdfId, formData).pipe(
+      catchError((error) => {
+        console.log("erreur");
+        return throwError(() => error);
+      })
+    )
+    .subscribe(
+      (response) => {console.log(response, "datas modifiées !"),
+    this.closeUpdateModale();
+    this.ngOnInit();
+    }
+    )
+  }
 }
 
-// Reactiveform (voir si je l'adapte)
-// Initialisation du formulaire d'update
-// this.pdfForm = this.formBuilder.group({
-//   author: ['', Validators.required],
-//   smallDescription: ['', Validators.required],
-//   description: ['', Validators.required],
-//   pdfFile: ['', Validators.required],
-// });

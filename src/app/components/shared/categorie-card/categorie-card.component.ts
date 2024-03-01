b96aadb8c12f3c596d1cfe5c61ebb2d078cfa8a1
Jerrylejer <jerrylejer23@yahoo.fr>
2024-03-01 17:44:20 +0100

    Ajout d'une requête dans categoryService pour lister les catégories selon une categorie parent => méthode ajoutée dans le back également, page-categorie envoie cette requête et affichera la future liste des catégories enfant + modification de categorie-list.component.html => j'enlève la boucle mise précédemment à ce niveau pour la mettre dans le composant enfant app- categorie-card.component.html => Plus logique + avant cela mes binding évènement & de propriété comme routerLink = aucun effet et pas de message erreur non plus ... Maintenat fonctionne correctement => J'affiche en console mes tableaux d'objets renfermant chaque categorie enfant pour une categorie parent cliquée

--- src/app/components/shared/categorie-card/categorie-card.component.ts
+++ src/app/components/shared/categorie-card/categorie-card.component.ts
@@ -1,16 +1,25 @@
 import { Component, Input, OnInit } from '@angular/core';
-import { catchError, throwError } from 'rxjs';
+import { Router } from '@angular/router';
 import { Category } from 'src/app/models/category.model';
-import { CategoryService } from 'src/app/services/category/category.service';
 
 @Component({
   selector: 'app-categorie-card',
   templateUrl: './categorie-card.component.html',
   styleUrl: './categorie-card.component.css'
 })
-export class CategorieCardComponent {
+export class CategorieCardComponent implements OnInit {
   // Je réceptionne la liste des catégories de categorie-list.component et stocke la liste dans categories
   // Dans le html, je boucle dessus pour accéder aux props de chacune des catégories et les afficher individuellement
-  @Input() categories!: Category;
+  @Input() categories: Category[] = [];
+
+  constructor(private route: Router) {}
+
+ ngOnInit(): void {
+
+ }
+
+ hello(): void {
+  console.log("hello")
+ }
 }
 
